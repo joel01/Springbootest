288c58f5f30a3077d9525eacc44065fa21f59d48
joel01 <jpolickm@hotmail.com>
2025-07-08 16:13:13 -0500

    Primer commit

---
+++ src/test/java/com/Joel/Springbootest/SpringbootestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Joel.Springbootest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
